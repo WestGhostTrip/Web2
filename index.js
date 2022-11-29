f11eb6b8be3452be4519dcf30a72cdaa6cd59c88
Антон Илларионов <anton.illarionov1991@gmail.com>
2022-11-29 17:25:30 +0300

    F1

---
+++ index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-debugger */
+const func = () => {
+  const sum = (a, b) => a + b;
+  let x = 10;
+  let y = 20;
+
+  // eslint-disable-next-line no-console
+  console.log(sum(x, y)); // 30
+  debugger;
+  x = 20;
+  y = 30;
+
+  // eslint-disable-next-line no-console
+  console.log(sum(x, y)); // 50
+};
+
+func();
